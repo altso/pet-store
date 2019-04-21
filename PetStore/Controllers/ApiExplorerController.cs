72047e435fb43b54f71050af30aac41c49013556
Alexander Tsoi <aliaksandr.tsoi@gmail.com>
2019-04-20 20:42:45 -0400

    Add basic ApiExplorer view

---
+++ PetStore/Controllers/ApiExplorerController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+
+namespace PetStore.Controllers
+{
+    [Route("api-explorer")]
+    [ApiExplorerSettings(IgnoreApi = true)]
+    public class ApiExplorerController : Controller
+    {
+        private readonly IApiDescriptionGroupCollectionProvider _apiExplorer;
+
+        public ApiExplorerController(IApiDescriptionGroupCollectionProvider apiExplorer)
+        {
+            _apiExplorer = apiExplorer;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(_apiExplorer);
+        }
+    }
+}
