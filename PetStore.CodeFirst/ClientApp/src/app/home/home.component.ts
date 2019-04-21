394e5119a0728c933c11896db84dff4076c60af2
Alexander Tsoi <aliaksandr.tsoi@gmail.com>
2019-04-21 18:59:26 -0400

    Show pets on home page

--- PetStore.CodeFirst/ClientApp/src/app/home/home.component.ts
+++ PetStore.CodeFirst/ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
+import { PetService } from 'src/api/generated/services';
+import { Pet } from 'src/api/generated/models';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
+
+  constructor(private petService: PetService) {
+    this.pets$ = petService.GetPets();
+  }
+
+  public pets$: Observable<Pet[]>;
 }
