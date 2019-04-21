33a0c0c3514f23d3e1ce87d6cc0e31fdbf443a9e
Alexander Tsoi <aliaksandr.tsoi@gmail.com>
2019-04-21 17:49:12 -0400

    Migrate to spa project template

---
+++ PetStore.CodeFirst/Controllers/PetController.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PetStore.Core;
+using PetStore.DataAccess;
+
+namespace PetStore.CodeFirst.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PetController : ControllerBase
+    {
+        private readonly IPetRepository _petRepository;
+
+        public PetController(IPetRepository petRepository)
+        {
+            _petRepository = petRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Pet>>> GetPets()
+        {
+            return await _petRepository.GetPets();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Pet>> GetPet(int id)
+        {
+            Pet pet = await _petRepository.GetPet(id);
+
+            if (pet == null)
+            {
+                return NotFound();
+            }
+
+            return pet;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Pet>> PutPet(int id, Pet pet)
+        {
+            if (id != pet.Id)
+            {
+                return BadRequest();
+            }
+
+            pet = await _petRepository.UpdatePet(pet);
+
+            if (pet == null)
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Pet>> PostPet(Pet pet)
+        {
+            await _petRepository.CreatePet(pet);
+            return CreatedAtAction("GetPet", new { id = pet.Id }, pet);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Pet>> DeletePet(int id)
+        {
+            Pet pet = await _petRepository.DeletePet(id);
+            if (pet == null)
+            {
+                return NotFound();
+            }
+
+            return pet;
+        }
+    }
+}
---
+++ PetStore.CodeFirst/Startup.cs
@@ -0,0 +1,96 @@
+using System.IO;
+using System.Reflection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PetStore.DataAccess;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace PetStore.CodeFirst
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            // In production, the Angular files will be served from this directory
+            services.AddSpaStaticFiles(configuration =>
+            {
+                configuration.RootPath = "ClientApp/dist";
+            });
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "Pet Store API", Version = "v1" });
+                c.DescribeAllEnumsAsStrings();
+                c.IncludeXmlComments(Path.ChangeExtension(Assembly.GetExecutingAssembly().CodeBase, ".xml"));
+                //c.EnableAnnotations();
+                //c.GeneratePolymorphicSchemas();
+            });
+
+            services.AddDbContextPool<PetStoreDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("PetStore"));
+            });
+            services.AddTransient<IPetRepository, PetRepository>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseSpaStaticFiles();
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Pet Store API v1");
+            });
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller}/{action=Index}/{id?}");
+            });
+
+            app.UseSpa(spa =>
+            {
+                // To learn more about options for serving an Angular SPA from ASP.NET Core,
+                // see https://go.microsoft.com/fwlink/?linkid=864501
+
+                spa.Options.SourcePath = "ClientApp";
+
+                if (env.IsDevelopment())
+                {
+                    //spa.UseAngularCliServer(npmScript: "start");
+                }
+            });
+        }
+    }
+}
